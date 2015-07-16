aeb48f260f97c28d28c01e107669c9e3205b36f4
bbouley <b.bouley@hotmail.co.uk>
2015-07-16 13:42:36 -0600

    edited code to use new variables

--- underscore.js
+++ underscore.js
@@ -7,7 +7,7 @@ var _ = { average : function(array){
 }
 
 var array1 = [1,2,3,4,5]
-console.log(_.average(array1))
+// console.log(_.average(array1))
 
 _.contains = function(array, num1){
   for (i=0; i < array.length; i++){
@@ -17,19 +17,19 @@ _.contains = function(array, num1){
   } return false;
 };
 
-console.log(_.contains(array1, 6))
+// console.log(_.contains(array1, 6))
 
 _.first = function(array){
   return array[0]
 }
 
-console.log(_.first(array1))
+// console.log(_.first(array1))
 
 _.last = function(array){
   return array[(array.length)-1]
 }
 
-console.log(_.last(array1))
+// console.log(_.last(array1))
 
 _.max = function(array){
   num = 0
@@ -40,7 +40,7 @@ _.max = function(array){
   } return num;
 }
 
-console.log(_.max(array1))
+// console.log(_.max(array1))
 
 _.min = function(array){
   num = 999999999
@@ -51,16 +51,23 @@ _.min = function(array){
   } return num;
 }
 
-console.log(_.min(array1))
+// console.log(_.min(array1))
 
 _.shuffle = function(array){
-  var m =array.length, t, i;
+  var m = array.length, t, i;
   while (m){
   i = Math.floor(Math.random() * m--);
   t = array[m];
   array[m]=array[i];
   array[i]=t;
-} return array;
+}  return array;
 };
 
 console.log(_.shuffle(array1))
+
+function updateDom(html,id){
+  var element = document.getElementById(id);
+  element.innerHTML = html;
+}
+
+updateDom('test','card')
